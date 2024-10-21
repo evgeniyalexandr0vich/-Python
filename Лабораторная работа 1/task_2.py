727dfe291ed1ed7a6ac15f26e59e4e54ef3fdbc1
evgeniyalexandr0vich <1511jeck@gmail.com>
2024-10-21 22:01:32 +0300

    Update task_2.py

--- Лабораторная работа 1/task_2.py
+++ Лабораторная работа 1/task_2.py
@@ -1,5 +1,12 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
 volume_of_disk = 1.44
-book_weight = (4 * 25 * 50 * 100) / (1024 ** 2)
-amount_of_books = volume_of_disk // book_weight
+symbol = 4
+quantity_of_symbols = 25
+quantity_of_lines = 50
+quantity_of_pages = 100
+kilobyte = 1024
+
+book_weight = symbol * quantity_of_symbols * quantity_of_lines * quantity_of_pages
+book_weight_megabyte = book_weight / kilobyte ** 2
+amount_of_books = volume_of_disk // book_weight_megabyte
 print("Количество книг, помещающихся на дискету:", int(amount_of_books))
