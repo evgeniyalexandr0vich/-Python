a48918645738252a6878e8d084b8426e1763295a
evgeniyalexandr0vich <1511jeck@gmail.com>
2024-11-16 23:36:59 +0300

    Update task_2.py

--- Лабораторная работа 4/task_2.py
+++ Лабораторная работа 4/task_2.py
@@ -7,15 +7,12 @@ OUTPUT_FILENAME = "output.json"
 
 
 def task() -> None:
-    data = []
     with open(INPUT_FILENAME, "r") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            data.append(row)
+        datas = [data for data in csv.DictReader(f)]
         # TODO считать содержимое csv файла
 
     with open(OUTPUT_FILENAME, 'w') as g:
-        json_data = json.dumps(data, indent=4)
+        json_data = json.dumps(datas, indent=4)
         return g.write(json_data)
         # TODO Сериализовать в файл с отступами равными 4
 
